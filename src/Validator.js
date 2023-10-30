31445dd2b25daa4b0eb9ffdf7f7319232563e306
Kr1tos <kritos.biz@gmail.com>
2023-10-30 22:09:02 +0300

    5/5

--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,16 @@
+import ArraySchema from "./ArraySchema.js"
+import stringSchema from "./StringSchema.js"
+import ObjectSchema from "./ObjectSchema.js";
 
+class Validator {
+    string() {
+        return new stringSchema()
+    }
+    array() {
+        return new ArraySchema([(value) => Array.isArray(value)]);
+    }
+    object() {
+        return new ObjectSchema();
+      }
+}
+export default Validator
